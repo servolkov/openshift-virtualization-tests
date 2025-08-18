6205de86d32b3d33166acfd39eeaa95aa07c63aa
Sergei Volkov <sevolkov@redhat.com>
2025-08-19 00:50:41 +0200

    extend functionality of cudn, nncp, nad classes
    New functionality:
    1) ClusterUserDefinedNetwork: the ability to create Layer2 CUDNs;
    2) NetworkAttachmentDefinition: class CNIPluginMacvlanConfig
    to create the macvlan NAD type;
    3) NodeNetworkConfigurationPolicy: class Vlan to create vlan
    interfaces on the cluster nodes;
    4) new function for udn\cudn NS creation;
    5) common change: the cached client usage.
    
    Signed-off-by: Sergei Volkov <sevolkov@redhat.com>

--- tests/network/libs/cluster_user_defined_network.py
+++ tests/network/libs/cluster_user_defined_network.py
@@ -1,5 +1,6 @@
 from dataclasses import asdict, dataclass
 from enum import Enum
+from typing import List
 
 from ocp_resources.cluster_user_defined_network import ClusterUserDefinedNetwork as Cudn
 
@@ -24,9 +25,22 @@ class Vlan:
 @dataclass
 class Ipam:
     class Mode(Enum):
+        ENABLED = "Enabled"
         DISABLED = "Disabled"
 
     mode: str
+    lifecycle: str | None = None
+
+
+@dataclass
+class Layer2:
+    class Role(Enum):
+        PRIMARY = "Primary"
+        SECONDARY = "Secondary"
+
+    role: str
+    ipam: Ipam
+    subnets: List[str]
 
 
 @dataclass
@@ -43,10 +57,12 @@ class Localnet:
 @dataclass
 class Network:
     class Topology(Enum):
+        LAYER2 = "Layer2"
         LOCALNET = "Localnet"
 
     topology: str
-    localnet: Localnet
+    layer2: Layer2 | None = None
+    localnet: Localnet | None = None
 
 
 class ClusterUserDefinedNetwork(Cudn):
@@ -55,10 +71,7 @@ class ClusterUserDefinedNetwork(Cudn):
     """
 
     def __init__(
-        self,
-        name: str,
-        namespace_selector: LabelSelector,
-        network: Network,
+        self, name: str, namespace_selector: LabelSelector, network: Network, label: dict[str, str] | None = None
     ):
         """
         Create and manage ClusterUserDefinedNetwork
@@ -71,11 +84,13 @@ class ClusterUserDefinedNetwork(Cudn):
             namespace_selector (NamespaceSelector): NamespaceSelector Label selector for which namespace network should
                 be available for.
             network (Network): Network is the user-defined-network spec.
+            label (dict[str, str]): Optional labels to apply to the ClusterUserDefinedNetwork.
         """
         super().__init__(
             name=name,
             namespace_selector=asdict(namespace_selector, dict_factory=dict_normalization_for_dataclass),
             network=asdict(network, dict_factory=dict_normalization_for_dataclass),
+            label=label,
         )
 
     class Status:
