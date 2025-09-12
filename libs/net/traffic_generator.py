75f2fcfb0277055ece7c4c424ac66b8391a1fd10
Sergei Volkov <sevolkov@redhat.com>
2025-09-12 16:54:24 +0200

    add traffic generator client for pod
    For some test suites, such as BGP, it is necessary
    to use a traffic generator inside the pod, rather than
    in a virtual machine. This commit adds a PodClient class
    that represents the tcp client running in the pod.
    
    Also this commit adds "--connect-timeout 0" option to
    iperf3 cmd to fix incorrect behavior and prevent false
    positives if there is no direct connectivity between
    the server and the client.
    
    Signed-off-by: Sergei Volkov <sevolkov@redhat.com>

--- libs/net/traffic_generator.py
+++ libs/net/traffic_generator.py
@@ -1,8 +1,9 @@
 import logging
 from typing import Final
 
+from ocp_resources.pod import Pod
 from ocp_utilities.exceptions import CommandExecFailed
-from timeout_sampler import TimeoutExpiredError, TimeoutSampler
+from timeout_sampler import TimeoutExpiredError, TimeoutSampler, retry
 
 from libs.vm.vm import BaseVirtualMachine
 
@@ -70,7 +71,7 @@ class Client:
         self._vm = vm
         self._server_ip = server_ip
         self._server_port = server_port
-        self._cmd = f"{_IPERF_BIN} --client {self._server_ip} --time 0 --port {self._server_port}"
+        self._cmd = f"{_IPERF_BIN} --client {self._server_ip} --time 0 --port {self._server_port} --connect-timeout 0"
 
     def __enter__(self) -> "Client":
         self._vm.console(
@@ -115,5 +116,51 @@ def _is_process_running(  # type: ignore[return]
         return False
 
 
-def is_tcp_connection(server: Server, client: Client) -> bool:
+class PodClient:
+    """Represents a TCP client that connects to a server to test network performance.
+
+    Expects pod to have iperf3 container.
+
+    Args:
+        pod (Pod): The pod where the client runs.
+        server_ip (str): The destination IP address of the server the client connects to.
+        server_port (int): The port on which the server listens for connections.
+        bind_interface (str): The interface or IP address to bind the client to (optional).
+            If not specified, the client will use the default interface.
+    """
+
+    def __init__(self, pod: Pod, server_ip: str, server_port: int, bind_interface: str | None = None):
+        self._pod = pod
+        self._server_ip = server_ip
+        self._server_port = server_port
+        self._container = _IPERF_BIN
+        self._cmd = f"{_IPERF_BIN} --client {self._server_ip} --time 0 --port {self._server_port} --connect-timeout 0"
+        self._cmd += f" --bind {bind_interface}" if bind_interface else ""
+
+    def __enter__(self) -> "PodClient":
+        # run the command in the background using nohup to ensure it keeps running after the exec session ends
+        self._pod.execute(
+            command=["sh", "-c", f"nohup {self._cmd} >/tmp/{_IPERF_BIN}.log 2>&1 &"], container=self._container
+        )
+        self._ensure_is_running()
+
+        return self
+
+    def __exit__(self, exc_type: BaseException, exc_value: BaseException, traceback: object) -> None:
+        self._pod.execute(
+            command=["pkill", "-f", self._cmd],
+        )
+
+    def is_running(self) -> bool:
+        out = self._pod.execute(command=["pgrep", "-f", self._cmd], ignore_rc=True)
+        return bool(out.strip())
+
+    @retry(wait_timeout=30, sleep=2, exceptions_dict={ProcessLookupError: []})
+    def _ensure_is_running(self) -> bool:
+        if self.is_running():
+            return True
+        raise ProcessLookupError(f"{_IPERF_BIN} client process did not start in the pod {self._pod.name}")
+
+
+def is_tcp_connection(server: Server, client: Client | PodClient) -> bool:
     return server.is_running() and client.is_running()
